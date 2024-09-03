a8a48cd909c9f8ec4887b75d871e61c91e933142
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-09-03 08:02:46 +0300

    refactor: Optimize shopping bag component and update cart total price calculation

--- src/app/components/Header/Shopingbag.tsx
+++ src/app/components/Header/Shopingbag.tsx
@@ -24,49 +24,50 @@ const Shopingbag = ({ setOpen }: { setOpen: (isOpen: boolean) => void }) => {
     return (
         <div className="mt-12 mb-4 mx-4 flex flex-col gap-4 overflow-hidden">
             <div className="text-lg ">Shopping bag</div>
+            {cart.length > 0 &&
 
-            <div className="overflow-auto">
-                <div>
-                    <div className="text-DarkGray text-xs">
-                        {cart.length > 1 ? `${cart.length} Items` : `${cart.length} Item`}
-                    </div>
-                    {cart.map((item) => (
-                        <div key={item.SKU} className="flex items-start gap-4 mt-2 mb-4">
-                            <Image width={120} height={120} src={item.image} alt={item.name} />
-                            <div className="flex flex-col">
-                                <div className="flex items-center justify-between font-medium gap-8">
-                                    <div>{item.name}</div>
-                                    <DeleteItems SKU={item.SKU} />
-                                </div>
-                                <div className="text-accent">{item.price * item.quantity} USD</div>
-                                <div className="text-sm justify-self-end text-DarkGray flex gap-2">
-                                    <div>QTY:</div>
-                                    <button onClick={() => handleDecrement(item.SKU)}>-</button>
-                                    <div>{item.quantity}</div>
-                                    <button onClick={() => handleIncrement(item)}>+</button>
+                <>
+                <div className="overflow-auto">
+                    <div>
+                        <div className="text-DarkGray text-xs">
+                            {cart.length > 1 ? `${cart.length} Items` : `${cart.length} Item`}
+                        </div>
+                        {cart.map((item) => (
+                            <div key={item.SKU} className="flex items-start gap-4 mt-2 mb-4">
+                                <Image width={120} height={120} src={item.image} alt={item.name} />
+                                <div className="flex flex-col">
+                                    <div className="flex items-center justify-between font-medium gap-8">
+                                        <div>{item.name}</div>
+                                        <DeleteItems SKU={item.SKU} />
+                                    </div>
+                                    <div className="text-accent">{item.price * item.quantity} USD</div>
+                                    <div className="text-sm justify-self-end text-DarkGray flex gap-2">
+                                        <div>QTY:</div>
+                                        <button onClick={() => handleDecrement(item.SKU)}>-</button>
+                                        <div>{item.quantity}</div>
+                                        <button onClick={() => handleIncrement(item)}>+</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
-            </div>
-
-
-
-            <div className="flex flex-col gap-4 justify-end ">
-                <hr className="border-t-2 border-Gray " />
-                <div className="text-sm flex justify-between items-center">
-                    <p>Subtotal {cart.length > 1 ? `${cart.length} Items` : `${cart.length} Item`}</p>
-                    <p>${totalPrice}</p>
-                </div>
-                {cart.length > 0 &&
-                <Link href="/Shopping-Card">
+                <div className="flex flex-col gap-4 justify-end ">
+                    <hr className="border-t-2 border-Gray " />
+                    <div className="text-sm flex justify-between items-center">
+                        <p>Subtotal {cart.length > 1 ? `${cart.length} Items` : `${cart.length} Item`}</p>
+                        <p>${totalPrice}</p>
+                    </div>
+                    <Link href="/Shopping-Card">
                         <button onClick={() => setOpen(false)} className="border w-full py-3 rounded-md font-semibold duration-200 border-black text-black hover:text-white hover:bg-black">
-                        VIEW CART
-                    </button>
-                </Link>
-                }
-            </div>
+                            VIEW CART
+                        </button>
+                    </Link>
+
+                </div>
+                </>
+            }
+            {cart.length === 0 && <div className="text-lg text-center text-accent font-bold">Your cart is empty</div>}
         </div>
     )
 }
