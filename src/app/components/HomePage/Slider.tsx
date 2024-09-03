899183f7c43b7dacbbd94cb5c078d42b7a7c5b40
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-09-03 09:49:05 +0300

    Optimize image loading in Slider component and add autoplay functionality

--- src/app/components/HomePage/Slider.tsx
+++ src/app/components/HomePage/Slider.tsx
@@ -1,17 +1,25 @@
 "use client";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import products from "@/app/static/products";
-import Card from "../Card/Card";
 import Image from "next/image";
 
 export default function SimpleSlider() {
     return (
-        <Swiper pagination={true} modules={[Pagination]} className="mySwiper flex justify-center items-center w-fit h-fit">
+        <Swiper
+            pagination={true}
+            modules={[Pagination, Autoplay]} // Include Autoplay module
+            className="mySwiper flex justify-center items-center w-fit h-fit"
+            autoplay={{
+                delay: 10000, // 5 seconds
+                disableOnInteraction: false, // Continue autoplay even after user interaction
+                reverseDirection: true, // Switch slides in reverse order
+            }}
+            loop={true} // Make sure the slider loops infinitely
+        >
             <SwiperSlide >
                 <Image src={'/HomePage/imgHome1.png'} alt="img" width={3000} height={500} className="w-full" />
             </SwiperSlide>
