87986cc16552cc6cef09180bdb83461b81fc5b84
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-09-04 11:34:49 +0300

    Optimize image loading and add lazy loading to ItemsCart component

--- src/app/components/Cart/ItemsCart.tsx
+++ src/app/components/Cart/ItemsCart.tsx
@@ -21,13 +21,13 @@ const ItemsCart = () => {
         <>
             {
                 cart.map((item) => (
-                    <div key={item.SKU} className="lg:grid grid-cols-3 gap-4 mb-4">
+                    <div key={item.SKU} className="grid grid-cols-3 gap-2 md:gap-4 mb-4 items-center">
                         <Image loading="eager" width={150} height={150} src={item.image} alt={item.name} />
                         <div className="flex flex-col gap-4">
                             <div className="font-">{item.name}</div>
                             <div className="text-accent">${item.price} USD</div>
                         </div>
-                        <div className="flex gap-8">
+                        <div className="flex md:gap-8 gap-4">
                             <div className="bg-LightGray text-DarkGray flex w-fit h-fit gap-4 px-3 py-3 rounded-md">
                                 <button onClick={() => handleDecrement(item.SKU)}>-</button>
                                 <div>{item.quantity}</div>
